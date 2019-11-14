e19938da229bf4aaf058abc1b3f4de0cf2593c35
Carlos Figueroa <cafb97@gmail.com>
2019-11-14 10:22:06 -0600

    First commit

---
+++ index.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+    #return "<h1>Hola Mundo!</h1>"
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+    #return "<h1>Acerca de...</h1>"
+
+@app.route('/articulos')
+def articulos():
+    return "<h1>Artículos</h1>"
+
+@app.route('/articulos/id')
+def lista_articulos():
+    return "<h1>Consulta de Artículos: {}</h1>".format(id)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "<h1>Página no encontrada</h1>"
+
+if __name__ == '__main__':
+    app.run(port=80, debug=True)
