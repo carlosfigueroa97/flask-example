681fddb82d25a1008d837fe5f568d7bf8e0a54d8
Carlos Figueroa <cafb97@gmail.com>
2019-11-29 16:27:14 -0600

    Add files to templates folder.

--- src/index.py
+++ src/index.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template("home.html")
-    #return "<h1>Hola Mundo!</h1>"
 
 @app.route('/about')
 def about():
     return render_template("about.html")
-    #return "<h1>Acerca de...</h1>"
+
+@app.route('/signUp')
+def sign_up():
+    return render_template("formulario.html")
+
+@app.route('/signUpUser', methods=['POST'])
+def sign_up_user():
+    name = request.form['nom']
+    ncontrol = request.form['trol']
+    return render_template("mensaje.html", nom=name, ncontrol=ncontrol)
 
 @app.route('/articulos')
 def articulos():
@@ -25,4 +33,4 @@ def page_not_found(error):
     return "<h1>Página no encontrada</h1>"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=80, debug=True)
